da340090418e4c2430daf4b050a494c125ea97da
Prakhar Dusad <prakhardusad15@gmail.com>
2026-05-25 19:02:13 +0530

    fix: cors and withCredentials

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'https://fintrack-z91l.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://fintrack-z91l.onrender.com/api' ,
+  withCredentials: true
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('fintrack_token');
