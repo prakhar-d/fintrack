0ecd14c9973eb1ae3ffe1cf9b0e1c4c24fa929de
Prakhar Dusad <prakhardusad15@gmail.com>
2026-05-25 15:29:26 +0530

    connected frontend to deployed backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://fintrack-z9l.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('fintrack_token');
